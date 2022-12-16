3d48aae828e6f2c06d47445949692feb3284aefd
Luisszzuniga <l.zuniga@webandcow.com>
2022-12-16 16:24:06 +0100

    Init back-office

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="flex h-screen bg-gray-50 dark:bg-gray-900"
+  >
+    
+    <Sidebar :toggleSideMenu="toggleSideMenu"/>
+
+    <div class="flex flex-col flex-1 w-full">
+      
+      <Header @toggleSideMenu="toggleSideMenu = ! toggleSideMenu"/>
+
+      <main class="h-full overflow-y-auto">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/Components/Admin/Header.vue";
+import Sidebar from "@/Components/Admin/Sidebar.vue";
+
+export default {
+  components: {
+    Header,
+    Sidebar
+  },
+
+  data() {
+    return {
+      toggleSideMenu: false
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
