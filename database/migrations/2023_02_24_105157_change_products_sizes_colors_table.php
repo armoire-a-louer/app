acf55216f61eb06b0a98ba5abef4e4630840e36a
Luisszzuniga <l.zuniga@webandcow.com>
2023-02-24 11:54:56 +0100

    MIGRATION: Change colors

---
+++ database/migrations/2023_02_24_105157_change_products_sizes_colors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products_sizes_colors', function (Blueprint $table) {
+            $table->renameColumn('color_id', 'primary_color_id');
+            $table->integer('secondary_color_id')->nullable()->after('color_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
