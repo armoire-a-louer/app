89d188fdbab16a8ba7021b3bce7e84e4a1156431
Luisszzuniga <l.zuniga@webandcow.com>
2023-06-09 13:44:51 +0200

    FEATURE: page concept

---
+++ resources/js/Components/PageInProgress.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="flex flex-col items-center justify-center">
+    <img src="/images/building_page.svg" alt="">
+
+    <h1 class="building-title mt-20">Oups ! Cette page n’est pas encore prête…</h1>
+
+    <p class="mt-3 text-center" style="width: 450px; max-width: 100%;">
+        Cette page se prépare avec soin pour vous offrir une expérience de mode luxueuse et raffinée. Revenez plus tard !
+    </p>
+
+    <Button class="mt-6" text="Retourner à l'accueil" color="black" textColor="white" :route="route('index')"/>
+  </section>
+</template>
+
+<script>
+import Button from './Buttons/Button.vue'
+
+export default {
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+.building-title {
+    font-size: 27px;
+    font-family: 'Roslindale', serif;
+    font-weight: bold;
+}
+</style>
--- resources/js/Pages/Concept.vue
+++ resources/js/Pages/Concept.vue
@@ -5,20 +5,22 @@
             <meta name="description" content="Bienvenue sur L'Armoire à Louer, votre destination en ligne pour louer des vêtements de marque de qualité. Découvrez notre vaste sélection de styles et louez votre tenue parfaite pour chaque occasion. Profitez de la flexibilité de la location de vêtements et élevez votre style sans effort.">
         </Head>
 
-        <h1>concept!</h1>
+        <PageInProgress class="mt-60 mb-20 "/>
     </Layout>
 </template>
 
 <script>
 import Layout from '@/Layouts/Layout.vue'
 import { Head } from '@inertiajs/inertia-vue3'
+import PageInProgress from '@/Components/PageInProgress.vue'
 
 export default {
     name: 'Concept',
     
     components: {
         Layout,
-        Head
+        Head,
+        PageInProgress
     }
 }
 </script>
